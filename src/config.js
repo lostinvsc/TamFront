d0baabf83772977a973a64fae8d77f5d61401586
sachin <sachinnagar2004@gmail.com>
2025-05-19 16:04:31 +0530

    sheet debug

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const backendURL = "http://localhost:10000";
-export const backendURL = "https://tambola-ppuw.onrender.com";
+export const backendURL = "http://localhost:10000";
+// export const backendURL = "https://tambola-ppuw.onrender.com";
